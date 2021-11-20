a1aa29c6701b9d6f6486c272160521754b8938c7
Brian Clark <clarkio@users.noreply.github.com>
2021-11-20 21:29:49 +0000

    remove commented code of old config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,6 @@
 // Full Astro Configuration API Documentation:
 // https://docs.astro.build/reference/configuration-reference
 
-// @type-check enabled!
-// VSCode and other TypeScript-enabled text editors will provide auto-completion,
-// helpful tooltips, and warnings if your exported object is invalid.
-// You can disable this by removing "@ts-check" and `@type` comments below.
-
-// @ts-check
-// export default /** @type {import('astro').AstroUserConfig} */ ({
-//   // Enable the Preact renderer to support Preact JSX components.
-//   renderers: ['@astrojs/renderer-svelte'],
-//   buildOptions: {
-//     /** Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs. */
-//     site: 'https://clarkio.com',
-//     /** Generate an automatically-generated sitemap for your build.
-//      * Default: true
-//      */
-//     sitemap: true,
-//   },
-// });
-
 export default {
   renderers: ['@astrojs/renderer-svelte'],
   buildOptions: {
