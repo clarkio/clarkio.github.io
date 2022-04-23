9ccafb3e0270bc2356c6646eea48a8768cdd569e
Brian Clark <clarkio@users.noreply.github.com>
2022-04-23 01:25:50 +0000

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 // Full Astro Configuration API Documentation:
 // https://docs.astro.build/reference/configuration-reference
 
+import svelte from '@astrojs/svelte';
+
 export default {
-  renderers: ['@astrojs/renderer-svelte'],
-  buildOptions: {
-    /** Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs. */
-    site: 'https://clarkio.com',
-    /** Generate an automatically-generated sitemap for your build.
-     * Default: true
-     */
-    sitemap: true,
-  },
+  integrations: [
+    svelte(),
+  ],
+  site: 'https://clarkio.com',
+  sitemap: false,
 }
