cd7773c1d23310f97ecc35138b8bb39abb3fe331
Dhanish <dhanish.b.gajjar@gmail.com>
2024-05-04 17:13:17 +0100

    moar!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // Full Astro Configuration API Documentation:
 // https://docs.astro.build/reference/configuration-reference
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://clarkio.com/',
-  integrations: [sitemap()],
-});
+  integrations: [sitemap()]
+})
