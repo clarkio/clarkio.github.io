daa8cf3b7d376a52546b9e4a0048f8b645f151bd
Dhanish <dhanish.b.gajjar@gmail.com>
2024-05-05 15:40:17 +0100

    added so much more

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 // Full Astro Configuration API Documentation:
 // https://docs.astro.build/reference/configuration-reference
-import { defineConfig } from 'astro/config'
+import { defineConfig, passthroughImageService } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://clarkio.com/',
-  integrations: [sitemap()]
+  integrations: [sitemap()],
+  image: {
+    domains: ['cdn.simpleicons.org'],
+    service: passthroughImageService(),
+    remotePatterns: [{ protocol: 'https' }]
+  }
 })
